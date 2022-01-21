a83dbbf09b65e918cb7bef9dab0e5c43f3d60de1
Полина Полянская <ppolina353@yandex.ru>
2022-01-21 20:56:14 +0300

    Добавьте файлы проекта.

---
+++ ThreadPoolDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Threading;
+
+class Program
+{
+    static void ShowMyText(object state)
+    {
+        string myText = (string)state;
+        Console.WriteLine("Thread: {0} - {1}", Thread.CurrentThread.ManagedThreadId, myText);
+        
+
+    }
+    static void Main(string[] args)
+    {
+        WaitCallback callback = new WaitCallback(ShowMyText);
+        ThreadPool.QueueUserWorkItem(callback, "iste");
+        ThreadPool.QueueUserWorkItem(callback, "kuzu kuzu");
+        ThreadPool.QueueUserWorkItem(callback, "geldim");
+        ThreadPool.QueueUserWorkItem(callback, "atese");
+        ThreadPool.QueueUserWorkItem(callback, "verdim");
+        ThreadPool.QueueUserWorkItem(callback, "yaktim");
+        Console.Read();
+    }
+
+}
